beb0d6c55630844ba2b302c3ec769a9b5dd92167
PieterWinderlichSchool <pieterwinderlich@outlook.com>
2018-10-08 10:24:51 +0200

    added chuncks and spawner

--- endlessRunner/Assets/scripts/animatorScript.cs
+++ endlessRunner/Assets/scripts/animatorScript.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class animatorScript : MonoBehaviour {
 
+
+	Animator animator;
 	// Use this for initialization
 	void Start () {
-		
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -31,29 +33,6 @@ public class animatorScript : MonoBehaviour {
 			animator.SetFloat("speed", 0);
 
 		}
-		if (Input.GetKey(KeyCode.LeftControl))
-		{
 
-			animator.SetBool("isCrouching", true);
-
-
-		}
-		else
-		{
-			animator.SetBool("isCrouching", false);
-		}
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-
-			animator.SetTrigger("jumps");
-
-
-
-
-		}
-		if (Input.GetKeyDown(KeyCode.X))
-		{
-			animator.SetTrigger("rolls");
-		}
 	}
 }
---
+++ endlessRunner/Assets/scripts/movementScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour {
+
+
+
+	public float speed = 0; 
+	public Rigidbody rb;
+	public int jumpForce = 5;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	// Update is called once per frame
+	void Update () {
+		float xInput = Input.GetAxisRaw ("Horizontal");
+
+		Vector3 moveDirection = new Vector3 (xInput, 0, 0);
+		transform.Translate (moveDirection * speed * Time.deltaTime);
+
+		if (Input.GetKey (KeyCode.D)) {
+			speed = 3;
+			if (Input.GetKey (KeyCode.Z)) {
+				speed = 6;
+			} else {
+				speed = 3;
+			}
+
+	}
+		else{
+			speed = 0;
+		}
+		if(Input.GetKey (KeyCode.Space)){
+			rb.AddForce(new Vector2(0, jumpForce));
+		}
+			
+}
+}
